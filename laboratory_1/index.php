d06c33d0fe6f99669815679f7363f2b14da59e95
Alex <alexyakimenkojr@gmail.com>
2025-02-13 11:07:15 +0200

    add laboratory_1

---
+++ laboratory_1/index.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "Hello, world";
+print "Hello, world <br>";
+
+$days = 288;
+$message = "Все возвращаются на работу!";
+
+echo $message . " Через " . $days . " дней <br>";
+echo "$message Через $days дней";
