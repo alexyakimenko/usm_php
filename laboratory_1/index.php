33f71daefc994966460b0f8220fed1b87dea16cb
Alex <alexyakimenkojr@gmail.com>
2025-02-18 21:20:17 +0200

    add laboratory_1 report as readme.md

--- laboratory_1/index.php
+++ laboratory_1/index.php
@@ -1,10 +1,14 @@
 <?php
 
-echo "Hello, world";
+echo "Hello, world!";
 print "Hello, world <br>";
 
 $days = 288;
 $message = "Все возвращаются на работу!";
 
 echo $message . " Через " . $days . " дней <br>";
-echo "$message Через $days дней";
+echo "$message Через $days дней\n";
+
+echo PHP_INT_MAX;
+
+?>
